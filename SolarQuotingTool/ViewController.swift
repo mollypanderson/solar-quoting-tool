088da3532680190f4a550d7eb8527f23202946db
mollypanderson <mollypanderson@yahoo.com>
2020-11-22 21:01:57 -0800

    Added basic calculation and UI for calculator view

--- SolarQuotingTool/ViewController.swift
+++ SolarQuotingTool/ViewController.swift
@@ -10,11 +10,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numSolarPanelsField: UITextField!
+    @IBOutlet weak var modulePicker: UIPickerView!
+    @IBOutlet weak var wattageField: UITextField!
+    @IBOutlet weak var inverterPicker: UIPickerView!
+    @IBOutlet weak var quoteResultField: UITextField!
+    
+    @IBAction func calculateButton(_ sender: UIButton) {
+        var solarPanels = Double(numSolarPanelsField.text!)
+        var wattage = Double(wattageField.text!)
+        
+        // initialize/defaults
+        if solarPanels == nil {
+            solarPanels = 0.0
+        }
+        if wattage == nil {
+            wattage = 0.0
+        }
+        
+        // calculation
+        let result = calculateQuote(solarPanelsIn: solarPanels ?? 0, wattageIn: wattage ?? 0)
+        quoteResultField.text = String(format: "$%.2f", result)
+    }
+    
+    @IBAction func contactSolXButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func shareQuoteButton(_ sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(dismissMyKeyboard))
+            
+            view.addGestureRecognizer(tap)
+        
+        numSolarPanelsField.delegate = self
+      //  modulePicker.delegate = self
+        wattageField.delegate = self
+       // inverterPicker.delegate = self
+        quoteResultField.delegate = self
     }
-
-
+    
+    @objc func dismissMyKeyboard() {
+        view.endEditing(true)
+    }
 }
+    
+    func calculateQuote(solarPanelsIn: Double, wattageIn: Double) -> Double {
+        let result = solarPanelsIn + wattageIn
+        return result
+    }
+    
+    extension ViewController : UITextFieldDelegate {
+        
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            textField.resignFirstResponder()
+            return true
+        }
+        
+    }
+    
+    
+
+
+
 
