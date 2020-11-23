a665cae55db3cb264cc5bcb12e6e80c5ff9ffd7f
mollypanderson <mollypanderson@yahoo.com>
2020-11-22 20:21:59 -0800

    Initial Commit

---
+++ SolarQuotingTool/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SolarQuotingTool
+//
+//  Created by Molly Anderson on 11/22/20.
+//  Copyright © 2020 Molly Anderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
